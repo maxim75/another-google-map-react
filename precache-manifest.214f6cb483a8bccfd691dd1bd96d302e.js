25f7be547dd150c6d58ae16205d98a9ebda19008
Maksym Kozlenko <max@kozlenko.info>
2021-03-09 17:40:49 +1100

    init

---
+++ precache-manifest.214f6cb483a8bccfd691dd1bd96d302e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "996b0607bcd88ef96d670f9e510b7027",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7c932f0784957c45f1c5",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "fe587159ec108f4ef6e0",
+    "url": "./static/js/2.599130ff.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.599130ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c932f0784957c45f1c5",
+    "url": "./static/js/main.b76e5b27.chunk.js"
+  },
+  {
+    "revision": "bd4620213105aeb33851",
+    "url": "./static/js/runtime-main.c5749fb0.js"
+  }
+]);
